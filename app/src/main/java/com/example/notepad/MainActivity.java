e69b85642c91c7e9b21ae0d0ede862e2c8b524ef
joeKonathapally <jk00764@surrey.ac.uk>
2019-02-14 16:21:23 +0000

    All good things must come to an end!!!

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.notepad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView uname;
+    private TextView pword;
+    private Button login;
+    private String username="";
+    private String password="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uname = (TextView)findViewById(R.id.username);
+        pword = (TextView)findViewById(R.id.password);
+
+        login = (Button)findViewById(R.id.login);
+
+
+    }
+
+
+    public void login(View view){
+
+        this.username=uname.getText().toString();
+        this.password=pword.getText().toString();
+        if(username.equals("admin") && password.equals("1234"))
+        {
+            startActivity(new Intent(MainActivity.this, Main2Activity.class));
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"",Toast.LENGTH_LONG).show();
+        }
+
     }
 }
