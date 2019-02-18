a17c53548b610128f821566d439c5f085e585bce
joeKonathapally <jk00764@surrey.ac.uk>
2019-02-18 15:09:04 +0000

    Figuring out the database stuff

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.notepad;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,33 +30,24 @@ public class MainActivity extends AppCompatActivity {
     private String password="";
     private HashMap<String,String> users;
     private ArrayList<String> usernames;
+    SQLiteDatabase mydatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         uname = (TextView)findViewById(R.id.username);
         pword = (TextView)findViewById(R.id.password);
+        mydatabase = openOrCreateDatabase("userData",MODE_PRIVATE, null);
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS Accounts(Username VARCHAR ,Password VARCHAR, PRIMARY KEY (Username));");
+        try{
+            mydatabase.execSQL("INSERT INTO Accounts VALUES('admin','1234');");
+        }
+        catch(Exception e){
 
+        }
         login = (Button)findViewById(R.id.login);
-        users = new HashMap<>();
-        usernames = new ArrayList<>();
-        users.put("admin","1234");
-        if(usernames.contains("admin"))
-        {
-
-        }
-        else
-        {
-            usernames.add("admin");
-        }
-        users.put(getIntent().getStringExtra("Username"),getIntent().getStringExtra("Password"));
-        usernames.add(getIntent().getStringExtra("Username"));
-
-
-
-
     }
 
 
@@ -54,20 +55,22 @@ public class MainActivity extends AppCompatActivity {
 
         this.username=uname.getText().toString();
         this.password=pword.getText().toString();
-        if(users.containsKey(username))
+        String query= "Select * from Accounts";
+        Cursor resultSet1 = mydatabase.rawQuery(query,null);
+        if(resultSet1.moveToFirst())
         {
-            if(password.equals(users.get(username)))
+            do
             {
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
-            }
-            else
-            {
-                startActivity(new Intent(MainActivity.this, Main3Activity.class));
-            }
+                if(resultSet1.getString(0)==username)
+                {
+                    if(resultSet1.getString(1).equals(password))
+                    {
+                        startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                    }
+                }
+            }while(resultSet1.moveToNext());
         }
-        else{
-            Toast.makeText(getApplicationContext(),"This username does not exist",Toast.LENGTH_LONG).show();
-        }
+
 
     }
 
@@ -77,5 +80,19 @@ public class MainActivity extends AppCompatActivity {
         stap.putStringArrayListExtra("usernames",usernames);
         startActivity(stap);
     }
+
+    private boolean isEmpty(Cursor resultSet)
+    {
+        for(int i=0;i<resultSet.getCount();i++)
+        {
+            if(resultSet.isNull(i)==false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
 }
